cbd77a983c41356fbcdd8fc7bc6eab81edc7fada
Dmitriy <Dmitriy@DESKTOP-8IVVVVB>
2022-10-22 22:23:29 +0300

    Добавьте файлы проекта.

---
+++ Task1/Linear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    struct Linear
+    {
+        double k;
+        double b;
+
+        public Linear(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        
+        public string Root()
+        {
+            double x = b / -k;
+            return $"x = {x}";
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Дано: 0=kx+b");
+            Console.WriteLine("Введите значение k");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение b");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Linear linear = new Linear(k, b);
+            Console.WriteLine(linear.Root());
+            Console.ReadKey();  
+        }
+    }
+}
